cdd2921302a083d9b8e847b8dd66ffd899080c62
Timothy Barnaby <tbarnaby1@gmail.com>
2023-06-30 21:40:43 -0500

    ipdated artist cards

--- client/src/pages/Artists.js
+++ client/src/pages/Artists.js
@@ -5,38 +5,44 @@ import { GET_ARTISTS } from '../utils/mutations';
 import './Artist.css'
 
 const Artists = () => {
-    // Query the artists data from the server
-    const { loading, error, data } = useQuery(GET_ARTISTS);
-  
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
-  
-    const artists = data.artists;
-  
-    return (
+  // Query the artists data from the server
+  const { loading, error, data } = useQuery(GET_ARTISTS);
 
-      <div>
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const artists = data.artists;
+
+  return (
+
+    <div>
       <h2>The Artists</h2>
-      <div id = 'artistPage'>
-        
-      {artists.map((artist) => (
-        <Link to={`/artist/${artist._id}`}> <div id = 'artistCard' key={artist._id}>
-        <img id = 'workImg'  src={artist.workImages[2]} alt='artist work' />
-        <div id = 'artistLink'>
-         {artist.name}
-          <p id="artistBioMain">{artist.bio}</p>
-        </div>
-        </div></Link>
+      <div id='artistPage'>
+
+        {artists.map((artist) => (
+          <Link to={`/artist/${artist._id}`}>
+            <div id="artistCard" key={artist._id}>
+              <div className="artistImage">
+                <img id="workImg" src={artist.workImages[2]} alt="artist work" />
+                <div className="artistName">{artist.name}</div>
+              </div>
+
+              <div className="artistDetails">
+
+                <div className="artistBioMain">{artist.bio}</div>
+              </div>
+            </div>
+          </Link>
         ))}
+
       </div>
-      </div>
-    );
-  };
-  
-  export default Artists;
-  
+    </div>
+  );
+};
+
+export default Artists;
